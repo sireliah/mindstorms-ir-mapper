2a476aca05515a1664f3bb1c35c36308ccb2f8fa
sireliah <sajuukk@gmail.com>
2018-12-31 19:08:35 +0100

    Complete sensor script.

--- sensor.py
+++ sensor.py
@@ -1,11 +1,13 @@
 
 from time import sleep
-from ev3dev2.sensor.lego import InfraredSensor
-
 from typing import Tuple
 import socket
 
+from ev3dev2.sensor.lego import InfraredSensor
+from ev3dev2.motor import MediumMotor, SpeedPercent, OUTPUT_D
+
 ADDRESS = ('192.168.0.11', 5000)
+INTERVAL = 0.1
 
 
 def send_data_to_server(data: str, address: Tuple) -> None:
@@ -15,12 +17,24 @@ def send_data_to_server(data: str, address: Tuple) -> None:
     print(response_data)
 
 
+def run_angle(motor: MediumMotor, speed: SpeedPercent, angle: int) -> None:
+    while motor.is_running:
+        sleep(INTERVAL)
+
+
 def get_prox() -> str:
+    speed = SpeedPercent(25)
+    degrees = 90
     sensor = InfraredSensor()
-    while True:
-        sleep(0.1)
-        prox = str(sensor.proximity)
-        send_data_to_server(prox, ADDRESS)
+    motor = MediumMotor(OUTPUT_D)
+
+    motor.on_for_degrees(speed, degrees, block=False)
+
+    while motor.is_running:
+        sleep(INTERVAL)
+        rotated_degrees = motor.count_per_rot
+        metrics = f'{rotated_degrees} {sensor.proximity}'
+        send_data_to_server(metrics, ADDRESS)
 
 
 if __name__ == '__main__':
